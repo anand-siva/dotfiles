b413ea61451db047230969513386fea173f070d2
Anand Siva <anand.siva@calltrackingmetrics.com>
2025-12-29 09:49:33 -0500

    need to fix up starship and git

--- install/common.sh
+++ install/common.sh
@@ -1,4 +1,14 @@
 #!/usr/bin/env bash
 set -e
 
+log "You will need to manually add the following for you final config"
+lb
+log "Create ~/.gitconfig.local:"
+cat <<'EOF'
+[user]
+  name = Anand Siva
+  email = anand.siva27@gmail.com
+  signingkey = <enter signing signingkey>
+EOF
+
 log "Make sure gpg is available"
