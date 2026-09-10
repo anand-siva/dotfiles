b0fc58cfaa4cf8ae53d3b5e6a6890beab2d02c5e
Anand Siva <anand.siva@calltrackingmetrics.com>
2026-09-10 14:26:21 -0400

    install tmux plugins

--- install/common.sh
+++ install/common.sh
@@ -1,6 +1,18 @@
 #!/usr/bin/env bash
 set -e
 
+log "Install tmux plugins"
+
+TPM_DIR="$HOME/.tmux/plugins/tpm"
+
+if [[ ! -d "$TPM_DIR" ]]; then
+  git clone https://github.com/tmux-plugins/tpm "$TPM_DIR"
+else
+  echo "Tmux Plugin Manager already installed, skipping"
+fi
+
+"$TPM_DIR/bin/install_plugins"
+
 log "You will need to manually add the following for you final config"
 lb
 log "Create ~/.gitconfig.local:"
